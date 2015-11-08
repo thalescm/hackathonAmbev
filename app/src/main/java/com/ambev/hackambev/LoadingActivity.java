00851da56b8ac42911b38240caf48f5e4cd77aec
Thales Machado <thales.mchd@gmail.com>
2015-11-08 03:00:09 -0200

    finished loading screen

--- app/src/main/java/com/ambev/hackambev/LoadingActivity.java
+++ app/src/main/java/com/ambev/hackambev/LoadingActivity.java
@@ -10,7 +10,36 @@ public class LoadingActivity extends Activity {
 
     private ImageView mLoadingBeer;
     private int mCount = 0;
-    private static final int mBeerAnim[] = {R.drawable.beer_loading_1, R.drawable.beer_loading_2, R.drawable.beer_loading_3, R.drawable.beer_loading_4};
+    private static final int mBeerAnim[] = {
+            R.drawable.drink08,
+            R.drawable.drink09,
+            R.drawable.drink10,
+            R.drawable.drink11,
+            R.drawable.drink12,
+            R.drawable.drink13,
+            R.drawable.drink14,
+            R.drawable.drink15,
+            R.drawable.drink16,
+            R.drawable.drink17,
+            R.drawable.drink18,
+            R.drawable.drink19,
+            R.drawable.drink20,
+            R.drawable.drink21,
+            R.drawable.drink22,
+            R.drawable.drink23,
+            R.drawable.drink24,
+            R.drawable.drink25,
+            R.drawable.drink26,
+            R.drawable.drink27,
+            R.drawable.drink28,
+            R.drawable.drink29,
+            R.drawable.drink30,
+            R.drawable.drink31,
+            R.drawable.drink32,
+            R.drawable.drink33,
+            R.drawable.drink34,
+            R.drawable.drink35
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +51,7 @@ public class LoadingActivity extends Activity {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                while (mCount < 30 && !isFinishing()) {
+                while (mCount < 100 && !isFinishing()) {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -33,7 +62,7 @@ public class LoadingActivity extends Activity {
 
                     mCount++;
                     try {
-                        Thread.sleep(200);
+                        Thread.sleep(50);
                     } catch (InterruptedException e) {
 
                     }
