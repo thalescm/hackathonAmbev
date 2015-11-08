68960f86ffde6ccec66a23ca386b17c939899e2a
Thales Machado <thales.mchd@gmail.com>
2015-11-08 09:24:25 -0200

    added touch to moods

--- app/src/main/java/com/ambev/hackambev/MoodActivity.java
+++ app/src/main/java/com/ambev/hackambev/MoodActivity.java
@@ -4,29 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MoodActivity extends AppCompatActivity {
 
-
-//    @Bind(R.id.brothersRL)
-//    View brothersRL;
-//    @Bind(R.id.dateRL)
-//    View dateRL;
-//    @Bind(R.id.crowdRL)
-//    View crowdRL;
-//    @Bind(R.id.soccerRL)
-//    View soccerRL;
-//    @Bind(R.id.wifiRL)
-//    View wifiRL;
-//    @Bind(R.id.musicRL)
-//    View musicRL;
-//    @Bind(R.id.foodRL)
-//    View foodRL;
-//    @Bind(R.id.parkRL)
-//    View parkRL;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -41,5 +26,102 @@ public class MoodActivity extends AppCompatActivity {
     public void findMyBar(View view) {
         Intent intent = new Intent(this, LoadingActivity.class);
         startActivity(intent);
+    }
+
+    @Bind(R.id.brothersI)
+    ImageView brothersI;
+    int brotherCT = 0;
+    int[] brotherArray = new int[] {
+            R.drawable.mood_brothers_gray,
+            R.drawable.mood_brothers_green,
+            R.drawable.mood_brothers_red};
+    @OnClick(R.id.brothersRL)
+    public void cheangeBrothers(View view) {
+        brothersI.setImageResource(brotherArray[++brotherCT % 3]);
+    }
+
+    @Bind(R.id.dateI)
+    ImageView dateI;
+    int dateCT = 0;
+    int[] dateArray = new int[] {
+            R.drawable.mood_date_gray,
+            R.drawable.mood_dare_green,
+            R.drawable.mood_date_red};
+    @OnClick(R.id.dateRL)
+    public void cheangeDates(View view) {
+        dateI.setImageResource(dateArray[++dateCT % 3]);
+    }
+
+    @Bind(R.id.crowdI)
+    ImageView crowdI;
+    int crowdCT = 0;
+    int[] crowdArray = new int[] {
+            R.drawable.mood_crowd_gray,
+            R.drawable.mood_crowd_green,
+            R.drawable.mood_crowd_red};
+    @OnClick(R.id.crowdRL)
+    public void cheangeCrowdss(View view) {
+        crowdI.setImageResource(crowdArray[++crowdCT % 3]);
+    }
+
+
+    @Bind(R.id.soccerI)
+    ImageView soccerI;
+    int soccerCT = 0;
+    int[] soccerArray = new int[] {
+            R.drawable.mood_soccer_gray,
+            R.drawable.mood_soccer_green,
+            R.drawable.mood_soccer_red};
+    @OnClick(R.id.soccerRL)
+    public void cheangeSoccers(View view) {
+        soccerI.setImageResource(soccerArray[++soccerCT % 3]);
+    }
+
+    @Bind(R.id.wifiI)
+    ImageView wifiI;
+    int wifiCT = 0;
+    int[] wifiArray = new int[] {
+            R.drawable.mood_wifi_gray,
+            R.drawable.mood_wifi_green,
+            R.drawable.mood_wifi_red};
+    @OnClick(R.id.wifiRL)
+    public void cheangeWifis(View view) {
+        wifiI.setImageResource(wifiArray[++wifiCT % 3]);
+    }
+
+    @Bind(R.id.musicI)
+    ImageView musicI;
+    int musicCT = 0;
+    int[] musicArray = new int[] {
+            R.drawable.mood_music_gray,
+            R.drawable.mood_music_green,
+            R.drawable.mood_music_red};
+    @OnClick(R.id.musicRL)
+    public void cheangeMusics(View view) {
+        musicI.setImageResource(musicArray[++musicCT % 3]);
+    }
+
+    @Bind(R.id.foodI)
+    ImageView foodI;
+    int foodCT = 0;
+    int[] foodArray = new int[] {
+            R.drawable.mood_food_gray,
+            R.drawable.mood_food_green,
+            R.drawable.mood_food_red};
+    @OnClick(R.id.foodRL)
+    public void cheangeFoods(View view) {
+        foodI.setImageResource(foodArray[++foodCT % 3]);
+    }
+
+    @Bind(R.id.parkI)
+    ImageView parkI;
+    int parkCT = 0;
+    int[] parkArray = new int[] {
+            R.drawable.mood_park_gray,
+            R.drawable.mood_park_green,
+            R.drawable.mood_park_red};
+    @OnClick(R.id.parkRL)
+    public void cheangeParks(View view) {
+        parkI.setImageResource(parkArray[++parkCT % 3]);
     }
 }
