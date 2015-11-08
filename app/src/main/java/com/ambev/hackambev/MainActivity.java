61244dbfdfc1b0bcae8d573885efaeaf65684b8b
Raphael Ferras <raphaelferras@gmail.com>
2015-11-08 09:14:49 -0200

    meLeva

--- app/src/main/java/com/ambev/hackambev/MainActivity.java
+++ app/src/main/java/com/ambev/hackambev/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ambev.hackambev;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.ambev.hackambev.bar.BarDetailActivity;
 
@@ -33,6 +35,9 @@ public class MainActivity extends AppCompatActivity
 
     @Bind(R.id.bar_card)
     CardView barCard;
+
+    @Bind(R.id.me_leve2)
+    Button meLeva;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,6 +111,13 @@ public class MainActivity extends AppCompatActivity
     @OnClick(R.id.bar_card)
     public void onClickBarDetail(View view) {
         Intent intent = new Intent(getApplicationContext(), BarDetailActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.me_leve2)
+    public void onClickMeLeva(View view) {
+        final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com.br/maps/dir/Ballroom+-+Rua+Estados+Unidos,+1570+-+Jardim+Paulista,+S%C3%A3o+Paulo+-+SP,+01412-100/Vila+Madalena,+S%C3%A3o+Paulo+-+State+of+S%C3%A3o+Paulo/@-23.5586979,-46.6920616,15z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x94ce59d64421f811:0xcbf2cb6d9ecbd226!2m2!1d-46.6690183!2d-23.566441!1m5!1m1!1s0x94ce57be69847fa9:0xd423d197dd9d1e01!2m2!1d-46.6975656!2d-23.5514369?hl=en"));
+        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
         startActivity(intent);
     }
 }
